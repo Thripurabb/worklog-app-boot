85ce9bfc406e577e460ffed5c5c44ea519ec2f11
Your Name <you@example.com>
2021-02-10 19:42:33 +0530

    second commit

--- ZuulGateway/src/main/groovy/com/worklog/ZuulGatewayApplication.groovy
+++ ZuulGateway/src/main/groovy/com/worklog/ZuulGatewayApplication.groovy
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
+import groovy.util.logging.Slf4j
+
+@Slf4j
 @SpringBootApplication
 @EnableZuulProxy
 class ZuulGatewayApplication {
 
 	static void main(String[] args) {
+		log.info("ZuulGateway application started")
 		SpringApplication.run(ZuulGatewayApplication, args)
 	}
 
