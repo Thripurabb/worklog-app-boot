7566d080e21d7fac0966d724f8eafe5a2416d703
Your Name <you@example.com>
2021-02-10 19:40:56 +0530

    second commit

--- EurekaDiscoveryServer/src/main/groovy/com/worklog/EurekaDiscoveryServerApplication.groovy
+++ EurekaDiscoveryServer/src/main/groovy/com/worklog/EurekaDiscoveryServerApplication.groovy
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
+import groovy.util.logging.Slf4j
+
+@Slf4j
 @SpringBootApplication
 @EnableEurekaServer
 class EurekaDiscoveryServerApplication {
 
 	static void main(String[] args) {
+		log.info("EurekaDiscoveryServer application started")
 		SpringApplication.run(EurekaDiscoveryServerApplication, args)
 	}
 
